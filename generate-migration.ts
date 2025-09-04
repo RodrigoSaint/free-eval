f7a65d0f776e39d2bf14cf4b132cb946ed00de92
Rodrigo Santos <rodsantosdasilva@gmail.com>
2025-09-04 17:25:34 +0200

    Creating migration

---
+++ generate-migration.ts
@@ -0,0 +1,9 @@
+import { migrate } from "drizzle-orm/libsql/migrator"
+import { db } from "./db.ts"
+
+console.log("Starting migration")
+await migrate(db, {
+  migrationsFolder: "./drizzle/",
+});
+console.log("Ending migration")
+
