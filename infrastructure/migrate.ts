fbc01a50a56a0292cecdfc24a9163fd41820d1e5
Rodrigo Santos <rodsantosdasilva+github@gmail.com>
2025-09-19 17:50:42 +0200

    testing path

--- infrastructure/migrate.ts
+++ infrastructure/migrate.ts
@@ -1,9 +1,9 @@
-import { migrate } from "drizzle-orm/libsql/migrator"
-import { db } from "./db.ts"
+import { migrate } from "drizzle-orm/libsql/migrator";
+import { db } from "./db.ts";
 
-export const createDbInstance = async ( ): Promise<void> => {
-    console.log("Starting migration")
-    console.log(import.meta)
-    await migrate(db, { migrationsFolder: `./drizzle/` });
-    console.log("Ending migration")
-}
+export const createDbInstance = async (): Promise<void> => {
+  const migrationsFolder = import.meta.resolve('../drizzle')
+
+  await migrate(db, { migrationsFolder });
+  console.log("Migration finished");
+};
