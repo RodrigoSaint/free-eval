1e207ecf766c51664b1967881da9676fb7376bfb
Rodrigo Santos <rodsantosdasilva+github@gmail.com>
2025-09-19 17:30:38 +0200

    Fixing migration

--- infrastructure/migrate.ts
+++ infrastructure/migrate.ts
@@ -1,8 +1,27 @@
-import { migrate } from "drizzle-orm/libsql/migrator"
-import { db } from "./db.ts"
+import { migrate } from "drizzle-orm/libsql/migrator";
+import { db } from "./db.ts";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-export const createDbInstance = async ( ): Promise<void> => {
-    console.log("Starting migration")
-    await migrate(db, { migrationsFolder: `${import.meta.dirname}/drizzle/` });
-    console.log("Ending migration")
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+let migrationsFolder: string;
+
+if (__dirname.includes("node_modules")) {
+  // file is inside a library
+  const parts = __dirname.split(path.sep);
+  const nodeModulesIndex = parts.lastIndexOf("node_modules");
+  const libRoot = path.join(...parts.slice(0, nodeModulesIndex + 2)); // +2 to include the library name
+  migrationsFolder = path.join(libRoot, "drizzle");
+} else {
+  // local migrations
+  migrationsFolder = path.join(__dirname, '..', "drizzle");
 }
+
+console.log(migrationsFolder)
+
+export const createDbInstance = async (): Promise<void> => {
+  console.log("Running migrations from:", migrationsFolder);
+  await migrate(db, { migrationsFolder });
+  console.log("Migration finished");
+};
