a30872b6a0594321f8d15f8a1bf9e4aaaaee8aa5
Rodrigo Santos <rodsantosdasilva+github@gmail.com>
2025-09-25 15:32:36 +0200

    Handling path normally

--- infrastructure/migrate.ts
+++ infrastructure/migrate.ts
@@ -2,7 +2,7 @@ import { migrate } from "drizzle-orm/libsql/migrator";
 import { db } from "./db.ts";
 
 export const createDbInstance = async (): Promise<void> => {
-  const migrationsFolder = import.meta.resolve('../drizzle')
+  const migrationsFolder = import.meta.resolve('./drizzle')
 
   await migrate(db, { migrationsFolder });
   console.log("Migration finished");
